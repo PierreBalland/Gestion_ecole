81e9919a22021885813a5b847721ffe35d9136bb
Pierre <pierre.balland@edu.ece.fr>
2019-06-08 02:14:31 +0200

    multiples ajouts : ajout personne, menu dans la menu barre commentaires, recherches dinformations , recherches de personnes ...

--- Gestion_ecole/src/vue/BadIdea1.java
+++ Gestion_ecole/src/vue/BadIdea1.java
@@ -10,6 +10,7 @@ package vue;
  * @author pierr
  * Classe inspiree du cours ActionListener
  */
+
 import mise_a_jour.*;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -20,56 +21,85 @@ import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JTextField;
+import java.util.*;
 
 
 public class BadIdea1 extends JFrame implements ActionListener, WindowListener {
     
 JButton b1 = new JButton("Button 1");
 JButton b2 = new JButton("Button 2");
+JButton b3 = new JButton("Button 3");
+JButton b4 = new JButton("Button 4");
 JTextField textbox1=new JTextField(); // les 3 textbox
 JTextField textbox2=new JTextField();
 JTextField textbox3=new JTextField();
 
 public BadIdea1()
 {
-setLayout(new FlowLayout());
-this.addWindowListener(this); // le BadIdea1 s'écoute lui-même!
-this.setSize(800,400);
-this.setLocationRelativeTo(null);
+    setLayout(new FlowLayout());
+    this.addWindowListener(this); // le BadIdea1 s'écoute lui-même!
+    this.setSize(800,400);
+    this.setLocationRelativeTo(null);
+    // Le menu avec la possibilité de sortir a tout moment
+    JMenuBar menuBar = new JMenuBar();
+    ImageIcon exitIcon = new ImageIcon("exit.png"); // creation de l'image 
 
-// Le menu avec la possibilité de sortir a tout moment
-JMenuBar menuBar = new JMenuBar();
-        ImageIcon exitIcon = new ImageIcon("exit.png"); // creation de l'image 
-
-        JMenu fileMenu = new JMenu("Exit"); // creation de longlet ? 
-        fileMenu.setMnemonic(KeyEvent.VK_F); // on set un event potentiel ? ...
+    JMenu fileMenu = new JMenu("Exit"); // creation de longlet ? 
+    fileMenu.setMnemonic(KeyEvent.VK_F); // on set un event potentiel ? ...
    
-        JMenuItem eMenuItem = new JMenuItem("Exit", exitIcon); // on créé un item de menu
-        eMenuItem.setMnemonic(KeyEvent.VK_E);  
-        eMenuItem.setToolTipText("Exit application"); // set le texte qui saffiche avec le curseur
-        eMenuItem.addActionListener((event) -> System.exit(0));
+    JMenuItem eMenuItem = new JMenuItem("Exit", exitIcon); // on créé un item de menu
+    eMenuItem.setMnemonic(KeyEvent.VK_E);  
+    eMenuItem.setToolTipText("Exit application"); // set le texte qui saffiche avec le curseur
+    eMenuItem.addActionListener((event) -> System.exit(0));
 
-        fileMenu.add(eMenuItem);
-        menuBar.add(fileMenu);
+    fileMenu.add(eMenuItem);
+    menuBar.add(fileMenu);
 
-        this.setJMenuBar(menuBar);
+    this.setJMenuBar(menuBar);
         
-this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-       
-//b1.addActionListener(this); // le BadIdea1 écoute le bouton
-//b2.addActionListener(this); // le BadIdea1 écoute le bouton
-//this.add(b1);
-//this.add(b2);
-}
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+}
+/**
+ * fonction pour le module mise a jour 
+ */
 public void set_maj() // pour la fenetre du module maj
 {
         this.setTitle("Menu de mise à jour");
         b1.addActionListener(this); // le BadIdea1 écoute le bouton
         b1.setText("Ajouter personne");
+        b4.addActionListener(this);
+        b4.setText("Delete Personne");
         this.add(b1);
+        this.add(b4);
 }
-
+/**
+ * fonction pour le module recherche informations 
+ */
+public void set_recherche(){
+    this.setTitle("Recherche d'informations");
+    b3.addActionListener(this);
+    b3.setText("Rechercher une personne");
+    this.add(b3);
+}
+/**
+ * fonction pour la recherche de personne depuis module recherche informations
+ */
+public void set_recherche_personne(){
+    this.setTitle("Affichage des personnes");
+    Recherche_info recherche=new Recherche_info();
+    ArrayList<String> arraydepersonnes=recherche.affichage_personne();
+    for(int i=0;i<arraydepersonnes.size();i++)
+    {
+        JTextField textbox=new JTextField(); // on créé un textbox pour chaque row de personnes
+        textbox.setText(arraydepersonnes.get(i));
+        this.getContentPane().add(textbox);
+    }
+  
+}
+/**
+ * fonction pour l'ajout de personne dans la bdd , module maj
+ */
 public void set_ajout_personne()
 {
   this.setTitle("Ajout d'un élève/professeur");
@@ -90,36 +120,54 @@ public void set_ajout_personne()
   this.getContentPane().add(this.textbox3);
   
 }
-// relents de programmation procédurale
+// TOUTES LES ACTIONS ICI
 @Override
 public void actionPerformed(ActionEvent e)
 {
-Object source = e.getSource();
-if(source == b1)
-{
-    System.out.println("Button 1 pressed");
-    this.dispose(); // ON PEUT FAIRE VISIBLE FALSE POUR LA GARDER ET PAS AVOIR A LA CREER A CHAQUE FOIS 
-    BadIdea1 fenetre_maj=new BadIdea1();
-    fenetre_maj.set_ajout_personne();
-    fenetre_maj.setVisible(true);
-}
-else if(source == b2) // bouton ajout personne
-{
-System.out.println("Button 2 pressed");
-Maj maj=new Maj();
-if(this.textbox1.getText()!="Nom" && this.textbox2.getText()!="Prenom" && this.textbox3.getText()=="élève" || this.textbox3.getText()=="professeur")
-{
-maj.ajoutpersonne(this.textbox1.getText(), this.textbox2.getText(), this.textbox3.getText());
-}
-else
-{
-    this.dispose();
-    SimpleMenuEx gobacktomenu=new SimpleMenuEx();
-
-}
-}
-else
-System.out.println("Something else");
+    //on recupere la source 
+    Object source = e.getSource();  
+    if(source == b1)
+    {
+        System.out.println("Button 1 pressed");
+        this.dispose(); // ON PEUT FAIRE VISIBLE FALSE POUR LA GARDER ET PAS AVOIR A LA CREER A CHAQUE FOIS 
+        BadIdea1 fenetre_maj=new BadIdea1();
+        fenetre_maj.set_ajout_personne();
+        fenetre_maj.setVisible(true);
+    }
+    else if(source == b2) // bouton ajout personne
+    {
+        System.out.println("Button 2 pressed");
+        Maj maj=new Maj();
+        if(this.textbox1.getText()!="Nom" && this.textbox2.getText()!="Prenom" &&(this.textbox3.getText().equals("élève") || this.textbox3.getText().equals("professeur")))
+        {
+            maj.ajoutpersonne(this.textbox1.getText(), this.textbox2.getText(), this.textbox3.getText());
+        }
+        else
+        {
+            //on go back au menu si on a mal rempli l'ajout
+            this.dispose();
+            SimpleMenuEx gobacktomenu=new SimpleMenuEx();
+            gobacktomenu.setVisible(true);
+        }
+    }
+    if (source == b3) //RECHERCHE PERSONNE
+    {
+        System.out.println("Button 3 pressed");
+        this.dispose(); // ON PEUT FAIRE VISIBLE FALSE POUR LA GARDER ET PAS AVOIR A LA CREER A CHAQUE FOIS 
+        BadIdea1 fenetre=new BadIdea1();
+        fenetre.set_recherche_personne();
+        fenetre.setVisible(true);
+    }
+    if(source==b4) // DELETE PERSONNE 
+    {
+        System.out.println("Button 4 pressed");
+        this.dispose(); // ON PEUT FAIRE VISIBLE FALSE POUR LA GARDER ET PAS AVOIR A LA CREER A CHAQUE FOIS 
+        BadIdea1 fenetre=new BadIdea1();
+        fenetre.set_recherche_personne();
+        fenetre.setVisible(true);
+    }
+    else
+        System.out.println("Something else");
 }
 
 @Override
@@ -142,11 +190,5 @@ public void windowDeactivated(WindowEvent e) {}
 @Override
 public void windowOpened(WindowEvent e) {}
 
-
-public static void main(String[] args) {
-JFrame f = new BadIdea1();
-f.setSize(300,200);
-f.setVisible(true);
-}
 
 } 
--- Gestion_ecole/src/vue/SimpleMenuEx.java
+++ Gestion_ecole/src/vue/SimpleMenuEx.java
@@ -28,15 +28,14 @@ import javax.swing.JMenuItem;
 public class SimpleMenuEx extends JFrame implements ActionListener {
     JButton b1 = new JButton("Module : Mise à jour");
     JButton b2 = new JButton("Module : Reporting");
+    JButton b3 = new JButton("Module : Recherche d'information");
     
     
     public SimpleMenuEx() {
-
         initUI();
     }
 
     private void initUI() {
-
         createMenuBar();
         setLayout(new FlowLayout());
         this.setTitle("Menu principal");
@@ -44,8 +43,10 @@ public class SimpleMenuEx extends JFrame implements ActionListener {
         this.setLocationRelativeTo(null);
         b1.addActionListener(this); // le BadIdea1 écoute le bouton
         b2.addActionListener(this);
+        b3.addActionListener(this);
         this.add(b1);
         this.add(b2);
+        this.add(b3);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         
     }
@@ -54,38 +55,53 @@ public class SimpleMenuEx extends JFrame implements ActionListener {
     public void actionPerformed(ActionEvent e)
     {
         Object source = e.getSource();
-    if(source == b1){
-    System.out.println("Button 1 pressed");
-    this.dispose(); // ON PEUT FAIRE VISIBLE FALSE POUR LA GARDER ET PAS AVOIR A LA CREER A CHAQUE FOIS 
-    BadIdea1 fenetre_maj=new BadIdea1();
-    fenetre_maj.set_maj();
-    fenetre_maj.setVisible(true);
-    }
-    if(source == b2)
-    {
-    System.out.println("Button 2 pressed");
-    }
-    else
-    System.out.println("Something else");
-  // a completer
+        if(source == b1){ // MISE A JOUR 
+            System.out.println("Button 1 pressed");
+            this.dispose(); // ON PEUT FAIRE VISIBLE FALSE POUR LA GARDER ET PAS AVOIR A LA CREER A CHAQUE FOIS 
+            BadIdea1 fenetre_maj=new BadIdea1();
+            fenetre_maj.set_maj();
+            fenetre_maj.setVisible(true);
+        }
+        if(source == b2)
+        {
+            System.out.println("Button 2 pressed");
+        }
+        if(source == b3) // RECHERCHE DINFO 
+        {
+            System.out.println("Button 3 pressed");
+            this.dispose(); // ON PEUT FAIRE VISIBLE FALSE POUR LA GARDER ET PAS AVOIR A LA CREER A CHAQUE FOIS 
+            BadIdea1 fenetre_maj=new BadIdea1();
+            fenetre_maj.set_recherche();
+            fenetre_maj.setVisible(true);
+        }
+        else
+            System.out.println("Something else");
+            // a completer
     }
 
+    /**
+     * Methode qui créé la barre de menu avec le exit et le menu qui permettent respectivement de quitter l'application et de revenir au menu principal 
+     */
     private void createMenuBar() {
 
         JMenuBar menuBar = new JMenuBar();
         ImageIcon exitIcon = new ImageIcon("exit.png"); // creation de l'image 
 
         JMenu fileMenu = new JMenu("Exit"); // creation de longlet ? 
+        JMenu fileMenu2 = new JMenu("Menu");
         fileMenu.setMnemonic(KeyEvent.VK_F); // on set un event potentiel ? ...
    
         JMenuItem eMenuItem = new JMenuItem("Exit", exitIcon); // on créé un item de menu
-        eMenuItem.setMnemonic(KeyEvent.VK_E);  
+        JMenuItem eMenuItem2 = new JMenuItem("Menu");
+        eMenuItem.setMnemonic(KeyEvent.VK_E); 
+        eMenuItem2.setMnemonic(KeyEvent.VK_E);
         eMenuItem.setToolTipText("Exit application"); // set le texte qui saffiche avec le curseur
         eMenuItem.addActionListener((event) -> System.exit(0));
-
+        eMenuItem2.addActionListener(this);
         fileMenu.add(eMenuItem);
+        fileMenu2.add(eMenuItem2);
         menuBar.add(fileMenu);
-
+        menuBar.add(fileMenu2);
         this.setJMenuBar(menuBar);
     }
     /*public static void main(String[] args) {
